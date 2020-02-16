c63ffbe99c61c0a07b3ee5d19817716a76afde39
Mayank Deshpande <43036566+mayank-pq2q4@users.noreply.github.com>
2020-02-16 15:04:23 +0530

    Update topologicalgraph.cpp

--- topologicalgraph.cpp
+++ topologicalgraph.cpp
@@ -12,7 +12,6 @@ struct Node
     int indegree;
     int data;
     struct Node *next;
-    struct Node *link[20];
 };
 
 typedef struct Node nde;
@@ -20,13 +19,17 @@ typedef struct Node nde;
 int priority[100];
 nde *adjacency[20];
 struct Node *head = NULL;
+
+
 void insert(int new_data)
 {
     struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
     new_node->data = new_data;
     new_node->next = head;
+    new_node->indegree = 0;
     head = new_node;
 }
+
 
 void append(int new_data)
 {
@@ -41,28 +44,35 @@ void append(int new_data)
     new_node->next = NULL;
     ptr->next = new_node;
 }
-void appendDegree(int node)
-{
-    struct Node *ptr;
-    ptr = head;
-    int i = 1;
-    while (i != node + 1)
-    {
-        cout << ptr->data << " ";
-        ptr = ptr->next;
-        i = i + 1;
-    }
-}
+
 void display()
 {
     struct Node *ptr;
     ptr = head;
     while (ptr != NULL)
     {
-        cout << ptr->data << " ";
+        cout << ptr->data << " indeg: ";
+        cout << ptr->indegree << ", ";
         ptr = ptr->next;
     }
+    std::cout << std::endl;
 }
+
+void appendDegree(int node)
+{
+    struct Node *ptr;
+    ptr = head;
+    int i = 1;
+    while (i < node)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+        i = i + 1;
+    }
+    ptr->indegree = ptr->indegree+1;
+    display();
+}
+
 int main()
 {
     int nodes;
@@ -83,34 +93,32 @@ int main()
     while (i <= nodes)
     {
         cout << "node: " << i << endl;
-        int ch;
+        int ch, node;
         int j = 0;
-        do
+        while (j < 20)
         {
             cout << "Enter choice: " << endl;
             cout << "1 if there exists a link to node " << i << endl;
             cout << "2 if there is no link anymore" << endl;
             cin >> ch;
-            switch (ch)
-            {
-            case 1:
+            if (ch==1)
             {
                 cout << "enter the node that it is linked to:" << endl;
                 cin >> node;
-                appendDegree(node);
+                appendDegree(node);                
             }
-
-            case 2:
+            else if (ch == 2)
             {
-                cout << "moving to the next node..." << endl;
+                std::cout << "moving on..." << std::endl;
                 break;
             }
-            }
             j = j + 1;
-        } while (j < 20);
+        }
 
         i = i + 1;
     }
+
+    display();
 
     return 0;
 }
